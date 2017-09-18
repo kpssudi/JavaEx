43ac99c2b8e3ecde5ce54fff8002ac6dbf93da88
kpssudi <kpssudi@gmail.com>
2017-09-18 16:53:12 -0700

    added p3_2 exercises updated

--- src/p3_2/CashRegister1.java
+++ src/p3_2/CashRegister1.java
@@ -12,11 +12,21 @@ public class CashRegister1 {
 	private double purchase;
 	private double payment;
 	private double tax_rate;
+	private double taxable_purchase;
+	private double total_tax;
 	
 	public CashRegister1(double fix_tax_rate) {
 		purchase = 0;
 		payment = 0;
 		tax_rate = fix_tax_rate;
+	}
+	public void recordTaxablePurchase(double amount){
+		double tax =taxable_purchase*tax_rate; 
+		total_tax = total_tax + tax;
+		taxable_purchase = taxable_purchase + tax;
+	}
+	public double getTotalTax(){
+		return total_tax;
 	}
 	public void recordPurchase(double amount){
 		purchase = purchase + amount;		
