dfd776f9616bf53708d1c1a8ed058587386ea88b
kp <kpssudi@gmail.com>
2017-10-04 16:57:40 -0700

    Updated MyStringVersion program

--- src/assign2a3/MyStringVersion.java
+++ src/assign2a3/MyStringVersion.java
@@ -18,15 +18,20 @@ public class MyStringVersion{
     private char[] charArray;
     
     public MyStringVersion(){
-    	
+    	this("");
     }
     public MyStringVersion(String inputJavaString){
     	charArray = new char[inputJavaString.length()];
-    	for(int i=0; i<inputJavaString.length(); i++){
+    	
+ /*   	for(int i=0; i<inputJavaString.length(); i++){
     		charArray[i] = inputJavaString.charAt(i);
-    	}
+    	}*/
+    	charArray = inputJavaString.toCharArray();
     }
-    
+    public MyStringVersion(MyStringVersion S){
+    	charArray = S.charArray;
+    }
+
     public MyStringVersion left(int size){
     	return new MyStringVersion(new String(charArray, 0, size));
     }
@@ -51,30 +56,52 @@ public class MyStringVersion{
     	return this.mid(1, charArray.length);    	    	
     }
     
+    public MyStringVersion subString(int startPos, int endPos){
+    	return new MyStringVersion(new String(charArray, startPos-1, endPos));   
+    }
+    public MyStringVersion charAtPos(int pos){
+    	return new MyStringVersion(new String(charArray, pos-1, 1));   
+    }
+        
+ /*   public MyStringVersion instr(String searchstring, int startPos){
+    	return new MyStringVersion(new String(charArray, searchstring, startPos-1));   
+    }*/
+    
+    public MyStringVersion invert(){
+    	char[] tempCharArray;
+    	int strLen = charArray.length;
+    	int counter = 0;
+    	tempCharArray = new char[charArray.length];
+    	for(int i=strLen; i<=0; strLen--){
+    		tempCharArray[counter] = charArray[i];
+    		counter++;
+    	}
+    	return new MyStringVersion(new String(tempCharArray));
+    }
     public String toString(){
     	return new String(this.charArray, 0, this.charArray.length); 
     	//return "test";   	
     }
-    
- /*   public MyStringVersion upperCase(){
+    public MyStringVersion upperCase(){
+    	int ascii = 0;
     	for(int i=0; i<charArray.length; i++){
-    		charArray[i] =Character.toUpperCase(charArray[i]);
+    		//charArray[i] =Character.toUpperCase(charArray[i]);
+    		ascii = charArray[i];
+    		if ((ascii >= 97) && (ascii <=122)) {
+    			charArray[i] -= 32;
+    		}
     	}
     	return this;
-    }*/
-    public MyStringVersion upperCase(){
-    	//char temp = ' ';
-    	for(int i=0; i<charArray.length; i++){
-    		//charArray[i] = Character.toUpperCase(charArray[i]);
-    		//temp = charArray.charAt(i);
-    		
-	}
-	return this;
-}
-    
+    }
+ 
     public MyStringVersion lowerCase(){
+    	int ascii = 0;
     	for(int i=0; i<charArray.length; i++){
-    		charArray[i] =Character.toLowerCase(charArray[i]);
+    		//charArray[i] =Character.toLowerCase(charArray[i]);
+       		ascii = charArray[i];
+    		if ((ascii >= 65) && (ascii <=90)) {
+    			charArray[i] += 32;
+    		}
     	}
     	return this;
     }
@@ -85,8 +112,8 @@ public class MyStringVersion{
     }
       
         
-    public static void main(String []args){
+/*    public static void main(String []args){
        MyStringVersion myString = new MyStringVersion("Hello world!");
        System.out.println(myString.lowerCase());
-    }
+    }*/
 }
--- src/assign2a3/MyStringVersionTester.java
+++ src/assign2a3/MyStringVersionTester.java
@@ -5,12 +5,30 @@ public class MyStringVersionTester {
 	public static void main(String[] args) {
 		String myString = new String("Computer");
 		MyStringVersion myText = new MyStringVersion(myString);
+		MyStringVersion myText1 = new MyStringVersion(myText);
+		
+		System.out.println("Using parameterized constructor:\n");
 		System.out.printf("Extracted text from left (default): %s\n", myText.left());
 		System.out.printf("Extracted text from left: %s\n", myText.left(3));
 		System.out.printf("Extracted text from right (default): %s\n", myText.right());
 		System.out.printf("Extracted text from right: %s\n", myText.right(3));
 		System.out.printf("Extracted text from mid (default): %s\n", myText.mid());
 		System.out.printf("Extracted text from mid: %s\n", myText.mid(2,5));
+		System.out.printf("Inverted text: %s\n", myText.invert());
+		System.out.printf("Substring from text: %s\n", myText.subString(4, 3));
+		System.out.printf("Character at position: %s\n", myText.charAtPos(3));
+		System.out.printf("Changing text to upper case: %s\n", myText.upperCase());
+		System.out.printf("Changing text to lower case: %s\n\n", myText.lowerCase());
+
+		System.out.println("Using copy constructor:\n");
+		System.out.printf("Extracted text from left (default): %s\n", myText1.left());
+		System.out.printf("Extracted text from left: %s\n", myText1.left(4));
+		System.out.printf("Extracted text from right (default): %s\n", myText1.right());
+		System.out.printf("Extracted text from right: %s\n", myText1.right(4));
+		System.out.printf("Extracted text from mid (default): %s\n", myText1.mid());
+		System.out.printf("Extracted text from mid: %s\n", myText1.mid(3,6));
+		System.out.printf("Changing text to upper case: %s\n", myText1.upperCase());
+		System.out.printf("Changing text to lower case: %s\n", myText1.lowerCase());
 		
 	}
 
